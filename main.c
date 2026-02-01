ddf87c2748551fe66860c92a98e3c3d4e39e601f
Raphael Hirtenfelder <hirtenfelder@bluewin.ch>
2026-02-01 17:55:08 +0100

    Init LCD

--- main.c
+++ main.c
@@ -4,9 +4,12 @@
 #include "pico/cyw43_arch.h"
 #include "pico/bootrom.h"
 #include "lib/cyw43_led.h"
+#include "wshare_lcd/DEV_Config.h"
+#include "wshare_lcd/GUI_Paint.h"
+#include "wshare_lcd/LCD_1in8.h"
 
-#define DEV_ENV
-#define DEV_ENV_RESET_TIMEOUT (30 * 1000)
+#define DEBUG_ENV
+#define DEBUG_ENV_RESET_TIMEOUT (30 * 1000)
 
 int64_t reset_pico() {
     printf("Entering BOOTSEL mode.\n");
@@ -18,8 +21,15 @@ int main(void) {
     cyw43_led_init();
     cyw43_led_enable();
 
-#ifdef DEV_ENV
-    add_alarm_in_ms(DEV_ENV_RESET_TIMEOUT, reset_pico, NULL, false);
+    if (DEV_Module_Init()) {
+        return EXIT_FAILURE;
+    }
+
+    LCD_1IN8_Init(HORIZONTAL);
+    LCD_1IN8_Clear(BLUE);
+
+#ifdef DEBUG_ENV
+    add_alarm_in_ms(DEBUG_ENV_RESET_TIMEOUT, reset_pico, NULL, false);
 #endif
 
     while (true) {
